2fd90a84842a2309d36bca8f1bba76a9e0632d29
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-10-06 21:27:14 +0200

    Updates

--- src/main/java/com/footballers/hoxtonjavafootballers/PlayerController.java
+++ src/main/java/com/footballers/hoxtonjavafootballers/PlayerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @RestController
 public class PlayerController {
@@ -39,7 +43,10 @@ class Player {
     public String nationality;
     public int scoreOutOfTen;
     public boolean isReplacement;
-    public int teamId;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "teamId", nullable = false)
+    public Team team;
 
     public Player() {
     }
